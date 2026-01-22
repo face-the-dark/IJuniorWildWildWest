b6de7bf7f94fe9063fb3b5863cdbafae90a4a876
Nikolai <ne360cosmos@gmail.com>
2026-01-22 23:37:48 +0300

    Добавлено управление анимациями

---
+++ src/IJuniorWildWildWest/Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerAnimator : MonoBehaviour
+{
+    private static readonly int IsWalkKey = Animator.StringToHash("IsWalk");
+    private static readonly int IsAiming = Animator.StringToHash("IsAiming");
+    private static readonly int VerticalKey = Animator.StringToHash("Vertical");
+    private static readonly int HorizontalKey = Animator.StringToHash("Horizontal");
+
+    [SerializeField] private PlayerInputReader _inputReader;
+    
+    private Animator _animator;
+
+    private Vector2 _direction;
+    private bool _isAimed;
+
+    private void Awake()
+    {
+        _animator =  GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        _inputReader.Moved += OnMoved;
+        _inputReader.Aimed += OnAimed;
+    }
+
+    private void OnDisable()
+    {
+        _inputReader.Moved -= OnMoved;
+        _inputReader.Aimed -= OnAimed;
+    }
+
+    private void OnMoved(Vector2 direction)
+    {
+        _direction = direction;
+
+        SwitchWalk();
+    }
+
+    private void OnAimed(bool isAimed)
+    {
+        _isAimed = isAimed;
+        _animator.SetBool(IsAiming, isAimed);
+        
+        SwitchWalk();
+    }
+
+    private void SwitchWalk()
+    {
+        if (_isAimed)
+        {
+            _animator.SetBool(IsWalkKey, false);
+            _animator.SetFloat(HorizontalKey, _direction.x);
+            _animator.SetFloat(VerticalKey, _direction.y);
+        }
+        else
+        {
+            _animator.SetBool(IsWalkKey, _direction.x != 0 || _direction.y != 0);
+        }
+    }
+}
